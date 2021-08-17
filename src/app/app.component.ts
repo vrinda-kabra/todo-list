d0d3821d485898251aca4d7cc7475f750f835a3d
K Vrinda <kvrinda1@gmail.com>
2021-08-17 20:19:09 +0530

    add app files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'smthng';
+  title = 'todo-list';
+  list:any[] = [];
+
+  add = document.getElementById("add");
+  
+  addTask(item:string){
+    this.list.push({id:this.list.length,name:item});
+    console.warn(this.list);
+    this.clearField()
+  }
+
+  deleteTask(id:number){
+    // console.log(id);
+    this.list = this.list.filter(item=>item.id!==id);
+  }
+  editTask(id:number){
+  }
+  clearField(){
+    var field = document.getElementById("input");
+    field!.innerHTML = '';
+  }
 }
